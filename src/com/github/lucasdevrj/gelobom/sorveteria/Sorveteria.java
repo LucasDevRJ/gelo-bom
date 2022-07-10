51f45a5f68b50bb0b1295c6af32bff610bf0b429
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-10 16:00:51 -0300

    Criação de mais métodos e alterações nos próprios

--- src/com/github/lucasdevrj/gelobom/sorveteria/Sorveteria.java
+++ src/com/github/lucasdevrj/gelobom/sorveteria/Sorveteria.java
@@ -1,12 +1,15 @@
 package com.github.lucasdevrj.gelobom.sorveteria;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.github.lucasdevrj.gelobom.cliente.Cliente;
+import com.github.lucasdevrj.gelobom.sorvete.Sorvete;
 
 public class Sorveteria {
 	
 	private String nome = "Gelo Bom";
+	private ArrayList<Sorvete> sorvetes = new ArrayList<Sorvete>();
 	
 	Scanner entrada = new Scanner(System.in);
 	
@@ -16,16 +19,20 @@ public class Sorveteria {
 		System.out.print("Digite seu nome: ");
 		cl.setNome(entrada.next());
 		
-		System.out.println("\nOlá " + cl.getNome() + ", seja bem-vindo(a) a nossa sorveteria " + this.getNome() + ".");
+		System.out.println("\nOlá " + cl.getNome() + ", seja bem-vindo(a) a nossa sorveteria " + this.getNome() + ".\n");
 		
 		exibeMenu();
 	}
 	
 	public void exibeMenu() {
+		System.out.println("----------|MENU|----------");
+		
 		System.out.println("Opção 1 - Ver cardápio.");
 		
-		System.out.println("Escolha a opção desejada: ");
+		System.out.print("Escolha a opção desejada: ");
 		int opcao = entrada.nextInt();
+		
+		System.out.println("---------------------------");
 		
 		switch (opcao) {
 			
@@ -35,17 +42,41 @@ public class Sorveteria {
 	}
 	
 	public void veCardapio() {
+		System.out.println("----------|CARDÁPIO|----------");
+		
 		System.out.println("Opção 1 - Sorvetes de Massa.");
+		System.out.println("Opção 2 - Sorvete Artesanal.");
+		
+		System.out.print("Digite a sua opção: ");
 		int opcao = entrada.nextInt();
 		
 		switch (opcao) {
 		case 1:
+			System.out.println("Sabores disponíveis:");
+			System.out.println("1 - Morango");
+			System.out.println("2 - Chocolate");
+			System.out.println("3 - Uva");
+			
+			System.out.println("Deseja comprar ou não?\n1 - Sim.\n2 - Voltar.");
+			int escolha = entrada.nextInt();
+			
+			if (escolha == 1) {
+				System.out.println("");
+			} else {
+				exibeMenu();
+			}
 			
 		}
+		System.out.println("------------------------------");
 		
+		exibeMenu();
 	}
 
 	public String getNome() {
 		return nome;
+	}
+	
+	public ArrayList<Sorvete> getSorvetes() {
+		return sorvetes;
 	}
 }
