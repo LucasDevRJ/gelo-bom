f8d770fd611f18ff07f3b4d6bd843c19e5471055
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-10 14:58:56 -0300

    Remoção dos atributos

--- src/com/github/lucasdevrj/gelobom/sorveteria/Sorveteria.java
+++ src/com/github/lucasdevrj/gelobom/sorveteria/Sorveteria.java
@@ -2,9 +2,5 @@ package com.github.lucasdevrj.gelobom.sorveteria;
 
 public class Sorveteria {
 
-	private String nome;
-	private Endereco endereco;
-	private String cor;
-	private double altura;
-	private double largura;
+	
 }
