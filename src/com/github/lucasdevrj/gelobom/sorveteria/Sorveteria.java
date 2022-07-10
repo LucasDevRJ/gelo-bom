dab94b647de8a42677f544ec5e2d3a44bede1e2c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-10 14:56:14 -0300

    Criação dos atributos da classe Sorveteria

--- src/com/github/lucasdevrj/gelobom/sorveteria/Sorveteria.java
+++ src/com/github/lucasdevrj/gelobom/sorveteria/Sorveteria.java
@@ -2,5 +2,9 @@ package com.github.lucasdevrj.gelobom.sorveteria;
 
 public class Sorveteria {
 
-	
+	private String nome;
+	private Endereco endereco;
+	private String cor;
+	private double altura;
+	private double largura;
 }
